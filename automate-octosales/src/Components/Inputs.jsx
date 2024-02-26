bb8289d6935e900decedbc413b0e2d3059a9d4c6
aliciachickenwings <91463251+AachalShrestha@users.noreply.github.com>
2024-02-26 13:28:31 +0100

    Html2pdf added

---
+++ automate-octosales/src/Components/Inputs.jsx
@@ -0,0 +1,75 @@
+import "../Styles/inputs.css";
+import React, { useState } from "react";
+import Contract from "../Components/Contract.jsx";
+
+const CreateContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    btw: "",
+    address: "",
+    representative: "",
+    tasks: "",
+    message: "",
+  });
+
+  const { name, btw, address, representative, tasks, message } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className="contract-wrapper">
+      <h3>New contract</h3>
+      <div className="contract-form-container">
+        <div className="contract-form-input-fields">
+          <input
+            type="text"
+            placeholder="Naam klant"
+            name="name"
+            value={name}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="BTW nummer"
+            name="btw"
+            value={btw}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Maatschappelijke zetel te ADRES KLANT"
+            name="address"
+            value={address}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Vertegenwoordiger naam"
+            name="representative"
+            value={representative}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Omschrijving takenpakket"
+            name="tasks"
+            value={tasks}
+            onChange={handleChange}
+          />
+        </div>
+        {message && <p className="message">{message}</p>}
+      </div>
+      <div className="contract-text-container">
+        <Contract data={formData} />
+      </div>
+    </div>
+  );
+};
+
+export default CreateContactForm;
---
+++ automate-octosales/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Inputs from "../Components/Inputs.jsx";
+import Text from "../Components/Contract.jsx";
+const App = () => {
+  return (
+    <div className="App">
+      <div className="forms-container">
+        <Inputs />
+      </div>
+    </div>
+  );
+};
+
+export default App;
